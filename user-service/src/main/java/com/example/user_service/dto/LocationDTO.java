7cc1bdcc2bf6b0d6080379a5c45684ad77bf59af
cmmohitchau <cmmohitchau@gmail.com>
2026-03-10 11:42:51 +0530

    added location dto

---
+++ user-service/src/main/java/com/example/user_service/dto/LocationDTO.java
@@ -0,0 +1,14 @@
+package com.example.user_service.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationDTO {
+
+    private double longitude;
+    private double latitude;
+
+}
