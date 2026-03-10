e1da84480b0a8200e197dbd90aba78a4153cfdc5
cmmohitchau <cmmohitchau@gmail.com>
2026-03-10 22:40:26 +0530

    added user preference service interface

--- user-service/src/main/java/com/example/user_service/service/UserPreferenceService.java
+++ user-service/src/main/java/com/example/user_service/service/UserPreferenceService.java
@@ -1,13 +1,19 @@
 package com.example.user_service.service;
 
 import com.example.user_service.dto.request.UserPreferenceRequestDTO;
-import com.example.user_service.dto.response.UserPreferenceResponseDTO;
+import com.example.user_service.dto.response.UserResponsePreferenceDTO;
 
 import java.util.UUID;
 
 public interface UserPreferenceService {
-    UserPreferenceResponseDTO createPreference(UserPreferenceRequestDTO dto);
-    UserPreferenceResponseDTO getPreferenceByUserId(UUID userId);
-    UserPreferenceResponseDTO updatePreference(UUID userId, UserPreferenceRequestDTO dto);
+
+    UserResponsePreferenceDTO createPreference(UserPreferenceRequestDTO requestDto);
+
+    UserResponsePreferenceDTO getPreferenceByUserId(UUID userId);
+
+    UserResponsePreferenceDTO updatePreference(UUID userId, UserPreferenceRequestDTO requestDto);
+
     void deletePreference(UUID userId);
+
+    boolean preferenceExists(UUID userId);
 }
