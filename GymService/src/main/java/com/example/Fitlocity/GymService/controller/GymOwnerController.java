3eaba711e11094f808165fe447529d1e94a46aff
Piyushkumarsingh1134 <piyushkumarsingh1134@gmail.com>
2026-03-28 12:46:23 +0530

    added api gateay

--- GymService/src/main/java/com/example/Fitlocity/GymService/controller/GymOwnerController.java
+++ GymService/src/main/java/com/example/Fitlocity/GymService/controller/GymOwnerController.java
@@ -3,6 +3,7 @@ package com.fitlocity.gym.controller;
 import com.fitlocity.gym.domain.GymOwner;
 import com.fitlocity.gym.service.GymOwnerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -14,11 +15,13 @@ public class GymOwnerController {
 
     private final GymOwnerService gymOwnerService;
 
+    @PreAuthorize("hasRole('OWNER')")
     @PostMapping
     public GymOwner createOwner(@RequestBody GymOwner owner) {
         return gymOwnerService.createOwner(owner);
     }
 
+    @PreAuthorize("hasRole('OWNER')")
     @GetMapping("/{id}")
     public GymOwner getOwner(@PathVariable UUID id) {
         return gymOwnerService.getOwner(id);
